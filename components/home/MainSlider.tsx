d4fc4d5302a5df60235489e55e95a600eda9bb8b
Ehsan Shafiee <esheshesh83@gmail.com>
2022-08-20 23:03:43 +0430

    index page slide show implemented

---
+++ components/home/MainSlider.tsx
@@ -0,0 +1,54 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import styles from "../../styles/components/home/mainSlider.module.scss";
+import Image from "next/image";
+import slide1 from "../../public/images/slide-1.jpg";
+import slide2 from "../../public/images/slide-2.jpg";
+import slide3 from "../../public/images/slide-3.jpg";
+import { Autoplay } from "swiper";
+import "swiper/css/pagination";
+import Link from "next/link";
+function MainSlider() {
+  return (
+    <Swiper
+      spaceBetween={15}
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: true,
+      }}
+      modules={[Autoplay]}
+      className={styles.swiper}
+    >
+      <SwiperSlide>
+        <div className={styles.swiperSlide}>
+          <Link href="/">
+            <a>
+              <Image src={slide1} placeholder="blur" />
+            </a>
+          </Link>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.swiperSlide}>
+          <Link href="/">
+            <a>
+              <Image src={slide2} placeholder="blur" />
+            </a>
+          </Link>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={styles.swiperSlide}>
+          <Link href="/">
+            <a>
+              <Image src={slide3} placeholder="blur" />
+            </a>
+          </Link>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default MainSlider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import MainSlider from "../components/home/MainSlider";
+import styles from "../styles/components/home/home.module.scss";
+import banner1 from "../public/images/ps5.jpg";
+import banner2 from "../public/images/xbox-x.jpg";
+import Link from "next/link";
 const Home: NextPage = () => {
   return (
-    <div className="container py-2" style={{height : 1000}}>خانه</div>
+    <div className="container py-2">
+      <div className={styles.gridArea}>
+        <div className={styles.sliderGrid}>
+          <MainSlider />
+        </div>
+        <div className={styles.boxesGrid}>
+          <Link href="/">
+            <a>
+              <Image src={banner1} placeholder={"blur"} />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <Image src={banner2} placeholder={"blur"} />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
