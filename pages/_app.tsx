762f469d5b35cfcc86c0a00a6ff9e6b14e685295
Ehsan Shafiee <esheshesh83@gmail.com>
2022-08-24 10:23:02 +0430

    progress bar added to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import Layout from "../components/MainLayout";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  NProgress.configure({ showSpinner: false });
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
   return (
     <Layout>
       <Component {...pageProps} />
